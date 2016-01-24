c110e76c1773bc1d510d18e88d9f010b7b243f23
ANDERSON MELLO <pazzamello@gmail.com>
2016-01-24 18:57:52 -0200

    removendo informacoes depreciadas do urls.py

--- clinicaMeiTan/urls.py
+++ clinicaMeiTan/urls.py
@@ -2,28 +2,23 @@ from django.conf.urls import patterns, include, url
 
 from django.conf import settings
 from django.contrib import admin
+from agenda import views
+from django.contrib.auth.views import logout_then_login, login
+from django.views.static import serve
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    #url(r'^$', 'clinicaMeiTan.views.home', name='home'),
-    url(r'^$',"agenda.views.lista" ),
-    url(r'^adiciona/$',"agenda.views.adiciona" ),
-    url(r'^item/(?P<nr_item>\d+)/$', "agenda.views.item"),
-    url(r'^remove/(?P<nr_item>\d+)/$', "agenda.views.remove"),
-    url(r'^login/$', "django.contrib.auth.views.login", {
-	    "template_name": "login.html" }),
-    url(r'^logout/$', "django.contrib.auth.views.logout_then_login", {
-	    "login_url": "/login/" }), 
-
-    # url(r'^clinicaMeiTan/', include('clinicaMeiTan.foo.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
-
+urlpatterns = [
+	url(r'^$',views.lista),
+	url(r'^adiciona/$',views.adiciona),
+	url(r'^item/(?P<nr_item>\d+)/$',views.item),
+	url(r'^remove/(?P<nr_item>\d+)/$',views.remove),
+	url(r'^login/$',login, {"template_name": "login.html"}),
+	url(r'^logout/$',logout_then_login, {"login_url": "/login/"}),
+	url(r'^admin/',include(admin.site.urls)), 
+	]
 
 if settings.DEBUG:
-	urlpatterns += patterns('',
-		(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-			{'document_root': settings.MEDIA_ROOT}),
-	)
+	urlpatterns += [
+	url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+	]
