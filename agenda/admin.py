723fc078e427c1b3c45e96516fae60c5476cfdc7
ANDERSON MELLO <pazzamello@gmail.com>
2016-02-12 22:59:35 -0200

    adicionando projeto ao projeto teste um modelo de pessoa

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from agenda.models import ItemAgenda
+from agenda.models import ItemAgenda, Person, Phone, Patient, HealthProfessional, Document
 from django.contrib import admin
 
 class ItemAgendaAdmin(admin.ModelAdmin):
@@ -14,5 +14,20 @@ class ItemAgendaAdmin(admin.ModelAdmin):
 	def save_model(self, request, obj, form, change):
 		obj.usuario = request.user
 		obj.save()
-
+class PersonAdmin(admin.ModelAdmin):
+    pass
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+class PatientAdmin(admin.ModelAdmin):
+    pass
+class HealthProfessionalAdmin(admin.ModelAdmin):
+    pass
+class DocumentAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(ItemAgenda, ItemAgendaAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(HealthProfessional, HealthProfessionalAdmin)
+admin.site.register(Document, DocumentAdmin)
+
--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -14,6 +15,56 @@ class ItemAgenda(models.Model):
 	def __unicode__(self):
 		return u"Titulo: %s Data/Hora:%s / %s" % (
 		self.titulo, self.data, self.hora)
+
+
+class Phone(models.Model):
+    CELULAR = 'CEL'
+    RESIDENCIAL = 'RES'
+    COMERCIAL = 'COM'
+    PHONE_TYPE_CHOICES = (
+            (CELULAR, 'Celular'),
+            (RESIDENCIAL, 'Residencial'),
+            (COMERCIAL, 'Comercial'),
+            )
+    phoneType = models.CharField(max_length=3,choices=PHONE_TYPE_CHOICES, default=CELULAR)
+    phoneNumber = models.CharField(max_length=20)
+
+
+class Document(models.Model):
+    CPF = 'CPF'
+    RG = 'RG'
+    CNH = 'CNH'
+    DOCUMENT_TYPE_CHOICES = (
+            (CPF, 'Cadastro Pessoa Física'),
+            (RG, 'Carteira de Identidade'),
+            (CNH, 'Carteira Nacional de Habilitação'),
+            )
+    documentType = models.CharField(max_length=3,choices=DOCUMENT_TYPE_CHOICES, default=CPF)
+    documentNumber = models.CharField(max_length=50)
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=100)
+    surname = models.CharField(max_length=200)
+    foneticName = models.CharField(max_length=300)
+    cpf = models.CharField(max_length=11, blank=True)
+    street = models.CharField(max_length=500)
+    number = models.CharField(max_length=10)
+    complement = models.CharField(max_length=50)
+    district  = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    uf = models.CharField(max_length=2, null=True)
+    cep = models.CharField(max_length=9, blank=True)
+    birthDate = models.DateField()
+    phone = models.ForeignKey(Phone, null=True)
+    document = models.ForeignKey(Document, null=True)
+
+class Patient(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    responsible = models.OneToOneField(Person, related_name='responsible', on_delete=models.SET_NULL, null=True)
+
+class HealthProfessional(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 def envia_email(**kwargs):
 	try:
